e5ece7116fa8c8161f4499a4f553bcbff8749d73
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-02 13:21:44 +0200

    mockup demo api

--- ilmoituslomake/ilmoituslomake/urls.py
+++ ilmoituslomake/ilmoituslomake/urls.py
@@ -29,6 +29,7 @@ urlpatterns += [
 
 # Notification Form App
 urlpatterns += [
+    path("api/demo/", notification_form_views.ApiDemoView.as_view()),
     path("api/hello/", notification_form_views.HelloView.as_view()),
     path(
         "api/schema/get/<int:id>/",
