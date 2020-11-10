f9b8d38e009871b87ba7bfefbb43a9d9b0f2688c
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-10 09:27:07 +0200

    Added Notification to Admin panel

--- ilmoituslomake/notification_form/admin.py
+++ ilmoituslomake/notification_form/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from base.models import Notification
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Notification, NotificationAdmin)
