f59cb96cd6758990927fcd862c25c520a5bcb7e8
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-10 16:22:17 +0200

    import ontologies command

--- local_dev/run_web.sh
+++ local_dev/run_web.sh
@@ -9,5 +9,7 @@ sleep 20
 python manage.py makemigrations users base
 # migrate db, so we have the latest db schema
 python manage.py migrate
+# load ontologies
+python manage.py import_ontology_words
 # start development server on public ip interface, on port 8008
 python manage.py runserver 0.0.0.0:8008
