37a48f6dfd7274d3db21de3573d357344085e376
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-17 14:49:48 +0200

    logout view

--- ilmoituslomake/users/views.py
+++ ilmoituslomake/users/views.py
@@ -1,5 +1,9 @@
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework import status
+from rest_framework.views import APIView
+from django.contrib.auth import logout
+
 from rest_framework.response import Response
 from rest_framework.generics import RetrieveAPIView
 from users.models import User
@@ -16,3 +20,14 @@ class UserView(RetrieveAPIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class UserLogout(APIView):
+    """
+    Logout the user
+    """
+
+    def get(self, request, format=None):
+        # using Django logout
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
