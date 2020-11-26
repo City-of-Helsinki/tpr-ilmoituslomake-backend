395e3373934ecf534eeff88906c013162553496e
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-11-26 11:21:20 +0200

    add signal for moderation app

---
+++ ilmoituslomake/moderation/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from notification_form.models import Notification
+from moderation.models import ModerationItem
+
+
+@receiver(post_save, sender=Notification)
+def create_moderation_item(sender, instance, **kwargs):
+    # Create ModerationItem if status is not approved or rejected
+    if not self.status in ["rejected", "approved"]:
+        moderation_item = ModerationItem(
+            target=self,
+            target_revision=self.revision,
+            category="moderation_task",
+            item_type=self.status,
+            data=self.data,
+        )
+    moderation_item.save()
