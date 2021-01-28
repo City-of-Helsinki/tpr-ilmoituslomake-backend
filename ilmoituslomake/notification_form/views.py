d22c97a5b6f8309b0331e3c2942dd895d3ca386c
karppanenr <9256992+karppanenr@users.noreply.github.com>
2021-01-28 12:45:27 +0200

    Update views.py

--- ilmoituslomake/notification_form/views.py
+++ ilmoituslomake/notification_form/views.py
@@ -40,7 +40,7 @@ class NotificationSchemaCreateView(CreateAPIView):
     # permission_classes =
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
