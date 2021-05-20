24bd790e80c868e2ec6412c274839d80fa5b80d0
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2021-05-20 15:51:53 +0300

    new ontology word implementation

--- ilmoituslomake/base/management/commands/import_ontology_words.py
+++ ilmoituslomake/base/management/commands/import_ontology_words.py
@@ -13,16 +13,20 @@ class Command(BaseCommand):
     # def add_arguments(self, parser):
     #    parser.add_argument('poll_ids', nargs='+', type=int)
 
-    def transform_data(self, data):
+    def transform_data(self, data, is_myhelsinki_keyword):
         return {
             "id": data["id"],
             "ontologyword": {
-                "fi": data["ontologyword_fi"],
-                "sv": data["ontologyword_sv"],
-                "en": data["ontologyword_en"],
+                "fi": data["ontologyword_fi"]
+                + (" (MyHelsinki)" if is_myhelsinki_keyword else ""),
+                "sv": data["ontologyword_sv"]
+                + (" (MyHelsinki)" if is_myhelsinki_keyword else ""),
+                "en": data["ontologyword_en"]
+                + (" (MyHelsinki)" if is_myhelsinki_keyword else ""),
             },
             "can_add_schoolyear": data["can_add_schoolyear"],
             "can_add_clarification": data["can_add_clarification"],
+            "is_myhelsinki_keyword": is_myhelsinki_keyword,
         }
 
     def handle(self, *args, **options):
@@ -37,6 +41,7 @@ class Command(BaseCommand):
             "type": "array",
             "items": {
                 "type": "object",
+                "additionalProperties": True,
                 "properties": {
                     "id": {"type": "number"},
                     "ontologyword_fi": {"type": "string"},
@@ -84,18 +89,34 @@ class Command(BaseCommand):
 
         try:
             # Making a get request
+            marketing_response = requests.get(
+                "https://www.hel.fi/palvelukarttaws/rest/v4/ontologyword/?category=tree0718"
+            )
             response = requests.get(
-                "https://www.hel.fi/palvelukarttaws/rest/v4/ontologyword/?category=MyHelsinki"
+                "https://www.hel.fi/palvelukarttaws/rest/v4/ontologyword/"
             )
 
+            # [A] Ensin lataa Marketing-asiasanat tästä (löytyy nyt 19 kpl):
+            # https://www.hel.fi/palvelukarttaws/rest/v4/ontologyword/?category=tree0718
+
+            # [B] Sitten lataa kaikki asiasanat tästä (löytyy nyt 990 kpl):
+            # https://www.hel.fi/palvelukarttaws/rest/v4/ontologyword/
+
+            # Jos [B]-alkio on [A]:ssa lisätään (MyHelsinki)
+
             # Parse JSON
+            marketing_json_data = marketing_response.json()
             json_data = response.json()
             # Validate against load schema
+            validate(instance=marketing_json_data, schema=ontology_load_schema)
             validate(instance=json_data, schema=ontology_load_schema)
             # Iterate through words (should be a list)
+            marketing_data = {data["id"]: data["id"] for data in marketing_json_data}
             save_array = []
             for word in json_data:
-                transformed_word = self.transform_data(word.copy())
+                transformed_word = self.transform_data(
+                    word.copy(), word["id"] in marketing_data
+                )
                 validate(instance=transformed_word, schema=ontology_save_schema)
                 save_array.append(transformed_word)
 
