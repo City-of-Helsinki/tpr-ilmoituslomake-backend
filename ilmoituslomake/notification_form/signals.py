9851ad5f5ae8971a76f7d74b4a490bfc291ebe1d
leekaun <leevi.kaunisto@cgi.com>
2021-11-08 15:08:30 +0200

    Add signals to opening_times.

---
+++ ilmoituslomake/notification_form/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from opening_times.utils import create_hauki_resource
+from notification_form.models import Notification
+
+@receiver(pre_save, sender=Notification)
+def pre_save_create_hauki_resource(sender, instance, **kwargs):
+    if instance.id is None:
+        resource = create_hauki_resource(instance.data['name'], instance.data['description']['short'], 
+                                        {"fi": instance.data['address']['fi']['street'], "sv": instance.data['address']['sv']['street'], 
+                                        "en": instance.data['address']['fi']['street']}, "unit", None, False, "Europe/Helsinki")
+        instance.hauki_id = resource["id"]
