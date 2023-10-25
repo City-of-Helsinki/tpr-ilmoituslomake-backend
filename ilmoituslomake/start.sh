8750d3fefacbb861969482d78223ed9525db47be
Anthony Brown <ant.brown@cgi.com>
2023-10-25 16:23:19 +0300

    Removed production script from test

--- ilmoituslomake/start.sh
+++
@@ -1,5 +0,0 @@
-echo "Run this as user tpr"
-source ../venv/bin/activate
-set -a; source ../.env; set +a;
-# cd ilmoituslomake && python manage.py runserver 0.0.0.0:8008 &
-gunicorn --bind=0.0.0.0:8008 ilmoituslomake.wsgi &
